50b8dee8ff4e50c8201a736408f68a26198c747d
Elliot Wright <wright.elliot@gmail.com>
2016-10-12 13:45:05 +0100

    Added some input types with todos, and output todos.

---
+++ input.go
@@ -0,0 +1,26 @@
+package console
+
+// Input represents the raw application input, in a slightly more organised way, and provides
+// helpers for retrieving that information. This allows commands to get application-wide input,
+// instead of only the command-specific input.
+type Input struct {
+	Arguments []InputArgument
+	Options   []InputOption
+}
+
+// InputArgument represents the raw data parsed as arguments, really this is just the value.
+type InputArgument struct {
+	Value string
+}
+
+// InputOption represents the raw data parsed as options. This includes it's name and it's value.
+// The value can be "" if no value is given (i.e. if the option is a flag).
+type InputOption struct {
+	Name  string
+	Value string
+}
+
+// @todo: Input:
+// @todo: - Add method for retrieving argument by index.
+// @todo: - Add method for retrieving option by name.
+// @todo: - Add method for retrieving option by names (first matching).
---
+++ output.go
@@ -0,0 +1,5 @@
+package console
+
+// @todo: Output:
+// @todo: - Allow specifying a writer to write to.
+// @todo: - Add Write, for writing strings to a the given writer.
