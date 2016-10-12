9f412db1a315c58e7aa545c7f86e28c99d7ba116
Elliot Wright <wright.elliot@gmail.com>
2016-10-12 22:57:14 +0100

    Updated the Command and Output types.

--- output.go
+++ output.go
@@ -1,5 +1,9 @@
 package console
 
+// Output abstracts application output. This is mainly useful for testing, as a different writer can
+// be passed to capture output in an easy to test manner.
+type Output struct{}
+
 // @todo: Output (low priority):
 // @todo: - Allow specifying a writer to write to.
 // @todo: - Add Write, for writing strings to a the given writer.
