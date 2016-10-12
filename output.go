4d6d113c84c171f97841df639638b219fd3898a2
Elliot Wright <wright.elliot@gmail.com>
2016-10-12 23:22:58 +0100

    Created constructor for Output.

--- output.go
+++ output.go
@@ -4,6 +4,10 @@ package console
 // be passed to capture output in an easy to test manner.
 type Output struct{}
 
+func NewOutput() *Output {
+	return &Output{}
+}
+
 // @todo: Output (low priority):
 // @todo: - Allow specifying a writer to write to.
 // @todo: - Add Write, for writing strings to a the given writer.
