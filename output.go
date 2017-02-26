3b35bd29bc95f41a422f827e53e011c19fb1c565
Elliot Wright <wright.elliot@gmail.com>
2017-02-26 23:26:57 +0000

    Allowed access to the writer on Output.

--- output.go
+++ output.go
@@ -8,31 +8,31 @@ import (
 // Output abstracts application output. This is mainly useful for testing, as a different writer can
 // be passed to capture output in an easy to test manner.
 type Output struct {
-	writer io.Writer
+	Writer io.Writer
 }
 
 // NewOutput creates a new Output.
 func NewOutput(writer io.Writer) *Output {
 	return &Output{
-		writer: writer,
+		Writer: writer,
 	}
 }
 
 // Print uses the fmt package's Print with a pre-set writer. Spaces are always added between
 // operands. It returns the number of bytes written and any write error encountered.
 func (o *Output) Print(a ...interface{}) (int, error) {
-	return fmt.Fprint(o.writer, a...)
+	return fmt.Fprint(o.Writer, a...)
 }
 
 // Printf uses the fmt package's Printf with a pre-set writer. It returns the number of bytes
 // written and any write error encountered.
 func (o *Output) Printf(format string, a ...interface{}) (int, error) {
-	return fmt.Fprintf(o.writer, format, a...)
+	return fmt.Fprintf(o.Writer, format, a...)
 }
 
 // Println uses the fmt package's Println with a pre-set writer. Spaces are always added between
 // operands and a newline is appended. It returns the number of bytes written and any write error
 // encountered.
 func (o *Output) Println(a ...interface{}) (int, error) {
-	return fmt.Fprintln(o.writer, a...)
+	return fmt.Fprintln(o.Writer, a...)
 }
