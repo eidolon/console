0015ba05a5b760ca663db5769e57f52a6fef0e3d
Elliot Wright <wright.elliot@gmail.com>
2017-03-19 18:59:57 +0000

    Pipeline template using yft to create template with all variables in.

--- ci/update.sh
+++ ci/update.sh
@@ -2,8 +2,17 @@
 
 SCRIPT_PATH="$(dirname "$0")"
 
+if test ! $(which yft); then
+    echo "Installing yft..."
+    go get -u github.com/SeerUK/yft/...
+fi
+
 pushd "$SCRIPT_PATH" > /dev/null
 
+# Create pipeline file, with variables substituted.
+yft pipeline.tpl.yml < variables.yml > pipeline.yml
+
+# Update pipeline in Concourse
 fly -t ci set-pipeline -p eidolon:console -c pipeline.yml -l variables.yml
 
 popd > /dev/null
