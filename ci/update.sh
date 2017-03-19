f568aa922bb643137e8a0e598ae672ad21d8f9af
Elliot Wright <wright.elliot@gmail.com>
2017-03-19 14:56:12 +0000

    WIP

--- ci/update.sh
+++ ci/update.sh
@@ -4,6 +4,6 @@ SCRIPT_PATH="$(dirname "$0")"
 
 pushd "$SCRIPT_PATH" > /dev/null
 
-fly -t ci set-pipeline -p eidolon:console -c pipeline.yml
+fly -t ci set-pipeline -p eidolon:console -c pipeline.yml -l variables.yml
 
 popd > /dev/null
