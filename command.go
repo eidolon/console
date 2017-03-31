6cfae74d5e8cc68363fff78934a0e08a56e636f5
Elliot Wright <wright.elliot@gmail.com>
2017-03-31 11:51:57 +0100

    Added struct field.

--- command.go
+++ command.go
@@ -16,6 +16,8 @@ type ExecuteFunc func(input *Input, output *Output) error
 type Command struct {
 	// The name of the command.
 	Name string
+	// An alias for the name, usually a shortened name.
+	Alias string
 	// The description of the command.
 	Description string
 	// Help message for the command.
